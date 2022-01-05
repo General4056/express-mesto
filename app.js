3189241511bd9e7e22e07847eba862663767cfe9
Denis Sapogov <General4056@yandex.ru>
2022-01-05 18:55:44 +0300

    добавлена обработка cors 1

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(requestLogger);
 
 app.use(cors);
+
 app.post('/signin', loginValidation, login);
 app.post('/signup', registrValidation, createUser);
 app.get('/signout', logout);
