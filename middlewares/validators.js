12d6f2b5db4e685f8927851af2f9af8fdacefeda
Denis Sapogov <General4056@yandex.ru>
2022-01-06 19:21:30 +0300

    добавлена обработка cors 6

--- middlewares/validators.js
+++ middlewares/validators.js
@@ -14,7 +14,7 @@ module.exports.cardValidation = celebrate({
     link: Joi.string()
       .required()
       .min(2)
-      .max(30)
+      .max(256)
       .custom(validateURL, 'custom validation'),
   }),
 });
