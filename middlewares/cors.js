1efa93e1ff6ef066af5852a849852176d36f60e9
Denis Sapogov <General4056@yandex.ru>
2022-01-07 14:37:24 +0300

    реализовно взаимодействие с фронтендом 5

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -10,19 +10,19 @@ const allowedCors = [
 const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
 
 module.exports.cors =  (req, res, next) => {
-  // const { origin } = req.headers;
-  // const { method } = req;
-  // const requestHeaders = req.headers['access-control-request-headers'];
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
 
-  // // if (allowedCors.includes(origin)) {
-  //   res.header('Access-Control-Allow-Origin', origin);
-  // // }
-  // if (method === 'OPTIONS') {
-  //   // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-  //   res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-  //   res.header('Access-Control-Allow-Headers', requestHeaders);
-  //   return res.end();
+  // if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', "*");
   // }
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
 
   next();
 }
